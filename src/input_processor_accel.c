675a309d9275eb8100ed7930443807843374bbac
Takahiro Natsume <tkn.infomail@gmail.com>
2025-07-27 15:10:50 +0900

    change accel event

--- src/input_processor_accel.c
+++ src/input_processor_accel.c
@@ -98,7 +98,9 @@ struct accel_data {
 
 static int accel_handle_event(const struct device *dev, struct input_event *event,
                              uint32_t param1, uint32_t param2,
-                             struct zmk_input_processor_state *state);
+                             struct zmk_input_processor_state *state) {
+                                return 1;
+                             }
 
 #define ACCEL_INST_INIT(inst)                                                  \
 static const uint16_t accel_codes_##inst[] = { INPUT_REL_X, INPUT_REL_Y, INPUT_REL_WHEEL, INPUT_REL_HWHEEL };     \
